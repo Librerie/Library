25894b2c3c277636ce39eea27ce9ebccf0dd280c
Marco Velluto <velluto93@gmail.com>
2014-05-20 21:25:03 +0200

    Initial Commit

---
+++ Library/Library.h
@@ -0,0 +1,13 @@
+//
+//  Library.h
+//  Library
+//
+//  Created by Marco Velluto on 20/05/14.
+//  Copyright (c) 2014 Marco Velluto. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Library : NSObject
+
+@end
---
+++ Library/Library.m
@@ -0,0 +1,13 @@
+//
+//  Library.m
+//  Library
+//
+//  Created by Marco Velluto on 20/05/14.
+//  Copyright (c) 2014 Marco Velluto. All rights reserved.
+//
+
+#import "Library.h"
+
+@implementation Library
+
+@end
